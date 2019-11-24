d9e99505c2bf3970ad097824110f0d5368683335
Jash patel <43696490+Jashpatel1@users.noreply.github.com>
2019-11-25 02:26:54 +0530

    updated autocrop

--- autocrop.m
+++ autocrop.m
@@ -1,9 +1,9 @@
-function[BIN]=autocrop(l)
+function[CROPPED]=autocrop(BINARY)
 
-[a,b]=size(l);
+[a,b]=size(BINARY);
 for i=1:a
     for j=1:b
-        if l(i,j)==1
+        if BINARY(i,j)==1
                 x1(i)=j;
                 break;
         else
@@ -14,7 +14,7 @@ end
 
 for i=1:b
     for j=1:a
-        if l(j,i)==1
+        if BINARY(j,i)==1
                 y1(i)=j;
                 break;
         else
@@ -25,7 +25,7 @@ end
 
 for i=a:-1:1
     for j=b:-1:1
-        if l(i,j)==1
+        if BINARY(i,j)==1
                 x2(i)=j;
                 break;
         else
@@ -36,7 +36,7 @@ end
 
 for i=b:-1:1
     for j=a:-1:1
-        if l(j,i)==1
+        if BINARY(j,i)==1
                 y2(i)=j;
                 break;
         else
@@ -49,9 +49,9 @@ x11=min(x1);
 y11=min(y1);
 x22=max(x2);
 y22=max(y2);
-BIN=imcomplement(imcrop(l,[x11,y11,x22-x11,y22-y11]));
+CROPPED=imcomplement(imcrop(BINARY,[x11,y11,x22-x11,y22-y11]));
 new_height=y22-y11;
 new_width=x22-x11;
 figure
-imshow(BIN);
+imshow(CROPPED);
 end
