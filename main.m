7fe95c39dbed622861a1648020465c3428456ab6
Jash patel <43696490+Jashpatel1@users.noreply.github.com>
2019-11-25 02:25:20 +0530

    updated main

--- main.m
+++ main.m
@@ -1,32 +1,36 @@
-RGB = imread('02101004.png');
+RGB = imread('00404004.png');
 figure
 imshow(RGB);
 DGRAY=grayscale(RGB);
-l=binarization(DGRAY);
-I=autocrop(l);
+BINARY=binarization(DGRAY);
+CROPPED=autocrop(BINARY);
+[PART]=divide(CROPPED);
 
-[new_height,new_width]=size(I);
+
+[new_height,new_width]=size(CROPPED);
 feature1_wtoh_ratio = new_width/new_height;
 feature2_high_pressure_area=0;
 for i=1:new_height
     for j=1:new_width
-        if I(i,j)==0
+        if CROPPED(i,j)==0
             feature2_high_pressure_area=feature2_high_pressure_area+1;
         end
     end
 end
 feature2_high_pressure_area=feature2_high_pressure_area/(new_height*new_width);
 
-feature3_projection_mean=mean(mean(I,2));
+feature3_projection_mean=mean(mean(CROPPED,2));
 
-feature4_vertical_std=std(mean(I,2));
-feature5_horizontal_std=std(mean(I,1));
+feature4_vertical_std=std(mean(CROPPED,2));
+feature5_horizontal_std=std(mean(CROPPED,1));
+
+
 
 binaryImage = true(size(DGRAY));
 measurements = regionprops(binaryImage, DGRAY, 'WeightedCentroid');
 feature6_cog = measurements(1).WeightedCentroid;
 
-JK=edge(I);
+JK=edge(CROPPED);
 feature9_high_pressure_area=0;
 for i=1:new_height
     for j=1:new_width
