c0cb9a8fd1be39ce93dcc27700e18ade42f16ace
Jash patel <43696490+Jashpatel1@users.noreply.github.com>
2019-11-25 19:06:49 +0530

    fixed convolute

--- convolve.m
+++ convolve.m
@@ -2,21 +2,22 @@ function convolute = convolve(A, k)
 [r, c] = size(A);
 [m, n, o] = size(k);
 
-for a=1:r
-    for b=1:c
+for a=1:r-m+1
+    for b=1:c-n+1
         AS(a,b)=0;
     end
 end
 
 for l=1:27
-    for i=1:r-8
-        for j=1:c-8
+    for i=1:r-m+1
+        for j=1:c-n+1
+            sum=0;
             for x=1:m
                 for y=1:n
-                    AS(i,j)=AS(i,j)+(A(x+i-1,y+j-1)*k(x,y,l));
+                    sum=sum+(A(x+i-1,y+j-1)*k(x,y,l));
                 end
             end
-            convolute(i,j,l)=(AS(i,j)/(m*n));
+            convolute(i,j,l)=(sum/(m*n));
         end
     end
 end
